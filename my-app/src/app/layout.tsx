72a9e14412382a5e3731608a709e6688d9b5e17f
손진경 <jk.son83@netive.co.kr>
2026-04-08 17:23:51 +0900

    로컬 폰트 설치 및 설정

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,16 +1,32 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const Pretendard = localFont({
+  src: [
+    {
+      path: "../font/Pretendard-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../font/Pretendard-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    }
+  ],
+  variable:"--font-sans",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// import { Geist, Geist_Mono } from "next/font/google";
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +39,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="ko">
+      <body className={`min-h-full flex flex-col ${Pretendard.variable}`} >{children}</body>
     </html>
   );
 }
