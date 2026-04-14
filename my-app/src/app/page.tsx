3b46be77fc8dd2c09d53583e8e558a45dafe7e10
손진경 <jk.son83@netive.co.kr>
2026-04-14 17:23:04 +0900

    tailwind 설치 및 적용테스트

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <>
       <h1>tailwind button 예제</h1>
       <section>
-        <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition" id="btn">버튼예제</button>
+        <button className="btn-primary" id="btn">버튼예제</button>
       </section>
     </>
   );
